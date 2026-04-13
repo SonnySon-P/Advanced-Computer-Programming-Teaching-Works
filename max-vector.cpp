fc319188f8763b17327ca0889c39d514fdac8471
SonnySon <58923633+SonnySon-P@users.noreply.github.com>
2026-04-13 18:03:37 +0800

    Update max-vector.cpp

--- max-vector.cpp
+++ max-vector.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-using namespace std;  // 命名空間，如果程式裡用到一些你沒看過的字眼，請先去std這個倉庫裡找找看
+using namespace std;
 
 int max(vector<int>& numbers) {
     if (numbers.empty()) {
@@ -22,7 +22,7 @@ int main() {
 
     int result = max(numbers);
 
-    std::cout << "數列中的最大值是: " << result << std::endl;
+    cout << "數列中的最大值是: " << result << endl;
 
     return 0;
 }
