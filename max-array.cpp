6f33f8b7948d92a3799de1244a186a2f44914e2c
SonnySon <58923633+SonnySon-P@users.noreply.github.com>
2026-04-13 17:47:01 +0800

    Add files via upload

---
+++ max-array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// 找出數列中的最大值
+int max(int array[], int size) {
+    int max_value = array[0]; 
+
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max_value) {
+            max_value = array[i];
+        }
+    }
+    return max_value;
+}
+
+int main() {
+    // 用Array定義一個數列
+    int numbers[] = {12, 45, 7, 89, 32, 56};
+
+    // 計算數列長度
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+
+    // 呼叫副程式
+    int result = max(numbers, n);  // 說明陣列退化問題，為何不能在副程式中在計算
+
+    std::cout << "數列中的最大值是: " << result << std::endl;
+
+    return 0;
+}
